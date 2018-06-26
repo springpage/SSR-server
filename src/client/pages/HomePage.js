690f501bc1673f89fe8015e23fafdf72931bf5b3
Spring Page <spring616666@gmail.com>
2018-06-26 15:22:36 -0700

    1

---
+++ src/client/pages/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home =() => {
+  return (
+    <div>
+      <h3 className="center-align" style={{marginTop: '200px'}}>
+        Welcome
+      </h3>
+      <p>Check out the awesome features</p>
+    </div>
+
+  )
+};
+
+export default {
+  component: Home,
+};
